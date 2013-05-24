89a94d8b8ba42d0b7a6f54eeb9b2491dd9db14df
ahmed elnaiem <aelnaiem@gmail.com>
2013-05-24 09:12:50 -0400

    changing spaces to tabs

--- main.go
+++ main.go
@@ -1,128 +1,128 @@
 package torronto
 
 import (
-  "fmt"
-  "io/ioutil"
-  "net"
-  "os"
-  "strings"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"strings"
 )
 
 func main() {
-  if len(os.Args) != 2 {
-    fmt.Fprintf(os.Stderr, "Usage: %s <host:port>", os.Args[0])
-    os.Exit(1)
-  }
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <host:port>", os.Args[0])
+		os.Exit(1)
+	}
 
-  addr := os.Args[1]
-  addrArr := strings.Split(addr, ':')
-  hostName, portNumber, err := addrAddr[0], Atoi(addrArr[1])
-  if len(peerData) != 2 {
-    fmt.Fprintf(os.Stderr, "Usage: %s <host:port>", os.Args[0])
-    os.Exit(1)
-  }
+	addr := os.Args[1]
+	addrArr := strings.Split(addr, ':')
+	hostName, portNumber, err := addrAddr[0], Atoi(addrArr[1])
+	if len(peerData) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <host:port>", os.Args[0])
+		os.Exit(1)
+	}
 
-  tcpAddr, err := net.ResolveTCPAddr("tcp4", addr)
-  checkError(err)
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", addr)
+	checkError(err)
 
-  listener, err := net.ListenTCP("tcp", tcpAddr)
-  checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
 
-  // read for input from stdin and do the appropriate action
-  // e.g. query for file, insert a file
-  go listenForInput()
+	// read for input from stdin and do the appropriate action
+	// e.g. query for file, insert a file
+	go listenForInput()
 
-  // listen for messages?
-  go listenForMessages()
+	// listen for messages?
+	go listenForMessages()
 
-  // instantiate peer and join network
-  peers := Peers{}
-  peers.initialize()
+	// instantiate peer and join network
+	peers := Peers{}
+	peers.initialize()
 
-  hostPeer = Peer {
-    currentState: Connected,
-    peers,        peers,
-    host:         hostName,
-    port:         portNumber,
-  }
+	hostPeer = Peer {
+		currentState: Connected,
+		peers,        peers,
+		host:         hostName,
+		port:         portNumber,
+	}
 
-  peer.Join()
+	peer.Join()
 }
 
 // Listen for user input to stdin
 func listenForInput() {
-  for {
-    var input string
-    _, err := fmt.Scanln(&input)
+	for {
+		var input string
+		_, err := fmt.Scanln(&input)
 
-    if err != nil {
-      inputArr := strings.Split(input, " ")
-      if strings.ToLower(inputArr[0]) == "insert" {
-        if len(inputArr) == 2 {
-          hostPeer.Insert(inputArr[1])
-        } else {
-          fmt.Fprintf(os.Stderr, "Usage: insert <filename>")
-        }
-      }
+		if err != nil {
+			inputArr := strings.Split(input, " ")
+			if strings.ToLower(inputArr[0]) == "insert" {
+				if len(inputArr) == 2 {
+					hostPeer.Insert(inputArr[1])
+				} else {
+					fmt.Fprintf(os.Stderr, "Usage: insert <filename>")
+				}
+			}
 
-      // TODO: accept other commands, like query...
-    }
-  }
+			// TODO: accept other commands, like query...
+		}
+	}
 }
 
 func listenForMessages() {
-  for {
-    conn, err := listener.Accept()
-    if err != nil {
-      continue
-    }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
 
-    // handle any new messages
-    go handleMessage(conn)
-  }
+		// handle any new messages
+		go handleMessage(conn)
+	}
 }
 
 // handle a message from a peer
 func handleMessage(conn net.Conn) {
-  
-  defer conn.Close()
 
-  //identify the type of message
-  //b is a byte array by default, holds the JSON msg passed
-  var m Message
-  err := json.Unmarshal(b, &m)
+	defer conn.Close()
 
- 
-  if (m.action = "join") {
-    //get hostName and portNumber and then...
-    hostPeer.Peers.connectPeer(m.HostName, m.PortNumber)
-  }
+	//identify the type of message
+	//b is a byte array by default, holds the JSON msg passed
+	var m Message
+	err := json.Unmarshal(b, &m)
 
-  if (m.action = "leave") {
-    hostPeer.Peers.disconnectPeer(m.HostName, m.PortNumber)
-  }
 
-  if (m.action = "files") {
-    // update status..
-  }
-  if (m.action = "upload") {
+	if (m.action = "join") {
+		//get hostName and portNumber and then...
+		hostPeer.Peers.connectPeer(m.HostName, m.PortNumber)
+	}
 
-  }
-  if (m.action = "download") {
+	if (m.action = "leave") {
+		hostPeer.Peers.disconnectPeer(m.HostName, m.PortNumber)
+	}
 
-  }
+	if (m.action = "files") {
+		// update status..
+	}
+	if (m.action = "upload") {
+
+	}
+	if (m.action = "download") {
+
+	}
 }
 
 // Possibly use a check error function to handle the many different
 // communication errors that can happen
 func checkError(err error) {
-  if err != nil {
-    // error handling
-  }
+	if err != nil {
+		// error handling
+	}
 }
 
 type Message struct {
-    HostName string
-    PortNumber string
-    Action string
+		HostName string
+		PortNumber string
+		Action string
 }
