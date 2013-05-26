f9d13f6529a058e0e72afa831f07bcbe84ca1357
apriyada <apriyada@gmail.com>
2013-05-26 09:51:24 -0400

    create_message: additional function to create msg
    returns the json message in the form of []byte and the length of the
    header in the form of int32. The header length value may not be
    calculated correctly (mentioned in the comments)

--- message.go
+++ message.go
@@ -8,8 +8,24 @@ type Message struct {
 	File       string
 }
 
-// Message should have a function to create a message given certain
-// values
 
-// Messages should be created to a specific 'header' size so it's easy
-// to tell what a message is vs. what a chunk is (for uploads)
+
+func create_message(hostName string, portNumber string, action string, files []string, file string) []byte, int32 {
+	//enter the information into the variable newMessage of type struct
+	newinformation := Message{
+		HostName: hostName,
+		PortNumber: portNumber,
+		Action: action,
+		Files: files,
+		File: file,
+	}
+	newMessage, err := json.Marshal(newinformation)
+
+	// return the header size so that it's easy
+	// to tell apart the header and the chunk/body
+	//not sure if commas, spaces and colons need to be included
+	headerLength = len("HostName") + len(hostName) + len("PortNumber") + len(portNumber) + len("Action") + len(action)
+
+	return newMessage, headerLength
+}
+
