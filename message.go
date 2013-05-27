cc38d7f15a06b6e76a59b3a729281d832bcf37f9
ahmed elnaiem <aelnaiem@gmail.com>
2013-05-26 22:41:56 -0400

    adding different functions as responses to different messages including request for files

--- message.go
+++ message.go
@@ -1,18 +1,18 @@
 package torronto
 
 type Message struct {
-	HostName   string
-	PortNumber int
-	Action     int
-	Files      []File
+	hostName   string
+	portNumber int
+	action     int
+	files      []File
 }
 
 func encodeMessage(hostName string, portNumber int, action int, files []File) []byte {
 	message := Message{
-		HostName:   hostName,   //hostName = 32bits
-		PortNumber: portNumber, //portNumber = 16 bits
-		Action:     action,     //store the action as a string or an int? action = 3 to 4 bits?
-		Files:      files,
+		hostName:   hostName,   //hostName = 32bits
+		portNumber: portNumber, //portNumber = 16 bits
+		action:     action,     //store the action as a string or an int? action = 3 to 4 bits?
+		files:      files,
 	}
 	jsonMessage, err := json.Marshal(message)
 
--- peer.go
+++ peer.go
@@ -12,7 +12,7 @@ type Peer struct {
 }
 
 func (peer Peer) Insert(filename String) int {
-	// add the file to the local node
+	// add the file to the local node and update status and filelist
 
 	// divide the file by chunks and push it out
 	// to peers
@@ -25,7 +25,7 @@ func (peer Peer) Query(status Status) int {
 func (peer Peer) Join() int {
 	filelist []File  := nil //shouldn't be nil, need to create the list of files here
 	//create the message to join
-	joinMessage:= create_message(peer.host, string(peer.port), 1, fileList)
+	joinMessage:= encodeMessage(peer.host, peer.port, Join, fileList)
 
 
 	// send out join message to all peers with your file list
@@ -39,8 +39,30 @@ func (peer Peer) Join() int {
 
 func (peer Peer) Leave() int {
 	// push out unique chunks, least replicated first
-
+	leaveMessage:= encodeMessage(peer.host, peer.port, Leave)
 	// close all sockets
+}
+
+func sendFileList(hostName string, portNumber int) {
+  // refer to either status object, or makeFileList() in file.go
+  // to obtain fileList
+  filesMessage:= encodeMessage(peer.host, peer.port, Files, fileList)
+}
+
+func sendFile(hostName string, portNumber int, file File) {
+	fileList := [1]File{File}
+	filesMessage:= encodeMessage(peer.host, peer.port, Upload, fileList)
+
+	// find the file and chunk in the directory, add it to the message and
+	// send .(file.fileName:file.chunks[0]) or chunk file.fileName to the
+	// appropriate part
+}
+
+func downloadFile(file File, conn net.Conn) {
+
+	// save the content to the file .(file.Name:file.chunks[0])
+
+	// if we have all the chunks, make the complete non-hidden file
 }
 
 const (
