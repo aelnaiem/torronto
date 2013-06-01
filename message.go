e924abb680232752ad890e9914777951ec1fa458
apriyada <apriyada@gmail.com>
2013-05-31 20:27:11 -0400

    removing comments

--- message.go
+++ message.go
@@ -19,22 +19,13 @@ func encodeMessage(hostName string, portNumber int, action int, files []File) []
 		files:      files,
 	}
 	jsonMessage, err := json.Marshal(message)
+	//error
 
-	//adds padding to message to make sure length = headerSize
 	if action == Upload {
-		//make a new array tempMessage with the desired length
 		tempMessage := make([]byte, HeaderSize, HeaderSize)
-		//copy contents of jsonMessage into tempMessage
-		//if len(jsonMessage) < len(tempMessage) then padding of 0s is added
-		//if len(jsonMessage) ? len(tempMessage) then only headerSize number of elements
-		//are copied over to tempMessage
 		copy(tempMessage, jsonMessage)
-		//make jsonMessage equals to the new array since this will be returned
 		jsonMessage = tempMessage
-		//use the len(jsonMessage) and cap(jsonMessage) functions to
-		//determine these values
 	}
-
 	return jsonMessage
 }
 
